6eeeee3c9b98405acbc9072f8f6e7c9ab99b2b8e
elsalvos <17753252+elsalvos@users.noreply.github.com>
2021-09-10 06:18:10 -0700

    Update test3.ps1

--- testing/test3.ps1
+++ testing/test3.ps1
@@ -1,4 +1,12 @@
-#cabrego - 20210412
-echo "hello worls"
+# cabrego - 20210412
+# Force use of TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Firewall
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
 
 write-host "successful"
+
+# Folders
+New-Item -ItemType Directory c:\temp1
+New-Item -ItemType Directory c:\music1
