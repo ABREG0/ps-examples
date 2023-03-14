c548c97ee098c14387972ff520041da6ee671d1b
elsalvos <17753252+elsalvos@users.noreply.github.com>
2023-03-14 11:23:33 -0700

    Update ado-agent.ps1

--- testing/ado-agent.ps1
+++ testing/ado-agent.ps1
@@ -1,10 +1,23 @@
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $poolName,
+    [Parameter()]
+    [string]
+    $adoOrgUrl,
+    [Parameter()]
+    [string]
+    $token
+)
+
 New-Item "C:\agent" -itemType Directory
-cd "C:\agent"
-$url = "https://dev.azure.com/YOUR_ORG"
-$token = "PAT_TOKEN"
+Set-Location "C:\agent"
+$ComputerName = hostname
 $auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$token"))
 
-$package = Invoke-RestMethod "$url/_apis/distributedtask/packages/agent?platform=win-x64&$`top=1" -Headers @{Authorization = "Basic $auth"}
+$package = Invoke-RestMethod "$adoOrgUrl/_apis/distributedtask/packages/agent?platform=win-x64&$`top=1" -Headers @{Authorization = "Basic $auth"}
 
 $fileName = $package.value[0].fileName;
 $downloadUrl = $package.value[0].downloadUrl;
@@ -15,5 +28,5 @@ Expand-Archive -Force agent.zip -DestinationPath .
 Remove-Item -Force agent.zip
 
 
-.\config.cmd --unattended --replace --acceptTeeEula --work work --url https://dev.azure.com/YOUR_ORG --pool YOUR_POOL_NAME --auth pat --token $token --runAsService --runAsAutoLogon --windowsLogonAccount USER --windowsLogonPassword USER_PASSWORD
+.\config.cmd --agetn $ComputerName --unattended --replace --acceptTeeEula --work work --url $adoOrgUrl --pool $poolName --auth pat --token $token --runAsService
 .\run.cmd
