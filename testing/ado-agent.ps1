3d6827c3e0462bf703511e52c453a5b0eb2ee5de
elsalvos <17753252+elsalvos@users.noreply.github.com>
2023-03-18 10:41:52 -0700

    Update ado-agent.ps1

--- testing/ado-agent.ps1
+++ testing/ado-agent.ps1
@@ -39,3 +39,37 @@ Invoke-WebRequest $package -Out agent.zip
 Expand-Archive -Path agent.zip -DestinationPath $PWD
 
 .\config.cmd --unattended --url "$($adoOrgUrl)" --pool "$($poolName)" --work "work" --agent "$($ComputerName)" --auth pat --token $token --runAsService --windowsLogonAccount 'NT AUTHORITY\NETWORK SERVICE'
+
+write-host "Check if PS was launched as admin..." -ForegroundColor Yellow
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = $SCRIPT:MyInvocation.MyCommand.Path 
+
+  write-host "check if PS was launched as admin... re-launching the script:  $($arguments) " -ForegroundColor Red
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  
+  Break
+} 
+ else{
+    Write-Host "PowerShell was Launched as Admin... " -ForegroundColor Green
+ }
+
+ $TLS12Protocol = [System.Net.SecurityProtocolType] 'Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $TLS12Protocol
+
+function Install-choco {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    
+
+}
+choco install powershell-core --version=7.2.1 -y --force --force-dependencies
+    
+choco install azure-cli -y --force --force-dependencies
+
+choco install git -y --force --force-dependencies
+
+choco install terraform --version=1.4.0 -y --force --force-dependencies
